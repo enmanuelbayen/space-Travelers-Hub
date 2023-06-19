11f49760b5caade1d64403d79a60264065a00b24
enmanuelbayen <enmanuel0495@gmail.com>
2023-06-19 14:18:19 -0400

    Linter fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,25 +6,49 @@ const Navbar = () => (
   <header className="header flex">
     <div className="nav-container flex">
       <p><TbPlanet className="userLogo" /></p>
-      <logo className="logo">Space Travelers' Hub</logo>
+      <logo className="logo">Space Travelers Hub</logo>
     </div>
     <nav className="nav flex">
       <NavLink
         to="/"
-        className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}
+        className={({ isActive, isPending }) => {
+          if (isPending) {
+            return 'pending';
+          }
+          if (isActive) {
+            return 'active';
+          }
+          return '';
+        }}
       >
         Rockets
       </NavLink>
       <NavLink
         to="/Missions"
-        className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}
+        className={({ isActive, isPending }) => {
+          if (isPending) {
+            return 'pending';
+          }
+          if (isActive) {
+            return 'active';
+          }
+          return '';
+        }}
       >
         Missions
       </NavLink>
       <span />
       <NavLink
         to="/MyProfile"
-        className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active' : '')}
+        className={({ isActive, isPending }) => {
+          if (isPending) {
+            return 'pending';
+          }
+          if (isActive) {
+            return 'active';
+          }
+          return '';
+        }}
       >
         My Profile
       </NavLink>
