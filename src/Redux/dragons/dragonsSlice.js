cf576d6adb0d68a123b444adc2d9c036fc1f5c90
Sergio Peralta <seperalta88@gmail.com>
2023-06-20 15:12:37 -0300

    Add dragons api endpoint

--- src/Redux/dragons/dragonsSlice.js
+++ src/Redux/dragons/dragonsSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchDragons = createAsyncThunk(
+  'dragons/fetchDragons',
+  async () => {
+    const response = await axios.get('https://api.spacexdata.com/v3/dragons');
+    return response.data;
+  },
+);
 
 const initialState = {};
 
