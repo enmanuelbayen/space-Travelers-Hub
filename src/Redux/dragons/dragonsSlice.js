9eabf01a2a3c68d4864632f1197bf3f3ba1f956e
Sergio Peralta <seperalta88@gmail.com>
2023-06-20 14:34:06 -0300

    Add dragons inicial setup

---
+++ src/Redux/dragons/dragonsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const dragonsSlice = createSlice({
+  name: 'dragons',
+  initialState,
+  reducers: {},
+});
+
+// export const {} = dragonsSlice.actions;
+
+export default dragonsSlice.reducer;
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missionSlice';
+import dragonsReducer from './dragons/dragonsSlice';
 
 export default configureStore({
   reducer: {
     missions: missionsReducer,
+    dragons: dragonsReducer,
   },
 });
