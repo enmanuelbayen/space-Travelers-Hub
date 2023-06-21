5248b674f58ecfd1e7734609db147040e737ee8a
Sergio Peralta <seperalta88@gmail.com>
2023-06-21 01:35:37 -0300

    Add dragons profile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,9 +1,42 @@
-import React from 'react';
+/* eslint-disable */
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>
-    <p>profile</p>
-  </div>
-);
+const MyProfile = () => {
+	const reservedDragons = useSelector((state) =>
+		state.dragons.dragons.filter((dragon) => dragon.reserved === true)
+	);
+
+	return (
+		<div className='profile-section'>
+			<div className='container row col-4'>
+				<h3>My Rockets</h3>
+				<ul className='list-group'>
+					<li className='list-group-item'>No Reserved Rockets</li>
+				</ul>
+			</div>
+
+			<div className='container row col-4'>
+				<h3>My Missions</h3>
+				<ul className='list-group'>
+					<li className='list-group-item'>No Missions</li>
+				</ul>
+			</div>
+			<div className='container row col-4'>
+				<h3>My Dragons</h3>
+				<ul className='list-group'>
+					{reservedDragons.length ? (
+						reservedDragons.map((dragon) => (
+							<li key={dragon.id} className='list-group-item'>
+								{dragon.name}
+							</li>
+						))
+					) : (
+						<li className='list-group-item'>No Reserved Dragons</li>
+					)}
+				</ul>
+			</div>
+		</div>
+	);
+};
 
 export default MyProfile;
